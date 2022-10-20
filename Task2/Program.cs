95f4dd7dca81328be22f31361dd517aeec0e8642
Elena Frolova <missis-fev@yandex.ru>
2022-10-20 16:49:37 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Программа, которая определяет какое число большее, а какое меньшее");
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (b > max) max = b;
+Console.Write("max= ");
+Console.WriteLine(max);
